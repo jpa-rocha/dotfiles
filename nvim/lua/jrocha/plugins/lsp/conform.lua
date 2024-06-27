e31fc2bb287c91668a7eeb5092a3009a25f91038
João Rocha <joao.rocha@wobcom.de>
2024-06-27 14:04:25 +0200

    fix: buffwritepr error

--- nvim/lua/jrocha/plugins/lsp/conform.lua
+++ nvim/lua/jrocha/plugins/lsp/conform.lua
@@ -34,38 +34,31 @@ return {
             format_on_save = function(bufnr)
                 -- Disable with a global or buffer-local variable
                 if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-                  return
-            end
+                    return
+                end
                 return { timeout_ms = 500, lsp_format = "fallback" }
             end,
-            })
+        })
 
-            vim.api.nvim_create_user_command("FormatDisable", function(args)
-                if args.bang then
+        vim.api.nvim_create_user_command("FormatDisable", function(args)
+            if args.bang then
                 -- FormatDisable! will disable formatting just for this buffer
-                    vim.b.disable_autoformat = true
-                else
-                    vim.g.disable_autoformat = true
-                end
-                print("Auto Format Disabled")
-            end, {
-                desc = "Disable autoformat-on-save",
-                bang = true,
-            })
+                vim.b.disable_autoformat = true
+            else
+                vim.g.disable_autoformat = true
+            end
+            print("Auto Format Disabled")
+        end, {
+            desc = "Disable autoformat-on-save",
+            bang = true,
+        })
 
-            vim.api.nvim_create_user_command("FormatEnable", function()
-                vim.b.disable_autoformat = false
-                vim.g.disable_autoformat = false
-                print("Auto Format Enabled")
-            end, {
+        vim.api.nvim_create_user_command("FormatEnable", function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+            print("Auto Format Enabled")
+        end, {
             desc = "Re-enable autoformat-on-save",
-            })
-        -- vim.keymap.set({ "n", "v" }, "<leader>F", function()
-        --     conform.format({
-        --         lsp_fallback = true,
-        --         async = false,
-        --         timeout_ms = 500,
-        --     })
-        -- end, { desc = "Format File/Range" })
+        })
     end
 }
